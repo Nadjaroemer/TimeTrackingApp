71bae1363a54b59a02263415df89f263b28116f0
Nadjaroemer <nadja.kuti@gmail.com>
2022-05-11 09:39:38 +0200

    Installing TailwindCss

---
+++ src/components/Kalender.js
@@ -0,0 +1,13 @@
+export default function Kalender() {
+  return (
+    <div className="flex justify-between">
+      <p>MON</p>
+      <p>TUE</p>
+      <p>WED</p>
+      <p>THU</p>
+      <p>FRI</p>
+      <p>SAT</p>
+      <p>SUN</p>
+    </div>
+  );
+}
